93a25d8fecd9456f959173c872a26a399a52646c
tonghoang <tongduchoang@gmail.com>
2021-12-03 22:45:01 +0700

    handle createProduct, updateProduct

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -109,8 +109,127 @@ module.exports = {
                     : parseInt(totalProduct.length / 15) + 1,
         });
     },
-    getProductByCategory: async (req, res) => {},
-    getProductById: async (req, res) => {},
+    getProductByCategory: async (req, res) => {
+        const { category } = req.params;
+        let { page, sort, max, min, type } = req.query;
+        sort = sort && parseInt(sort); // chuyển String qua int
+        page = page ? page : 1; // nếu không có query page thì trả về page 1
+        type = type ? type : "1" // nếu không có type thì trả về type 1
+        var filterPrice;
+        max ? filterPrice = { $lte: max } : {}; // kiểm tra nếu có max thì khởi tạo object max;
+
+        var listProducts, totalProduct; //khởi tạo biến toàn cục
+        if (category == "PK") {
+            if (sort) {
+                listProducts = await ProductModel.find({
+                    $and: [
+                        { category: { $ne: "Laptop" } },
+                        { type },
+                    ],
+                    newprice: {
+                        $gte: min ? min : 0,
+                        ...filterPrice
+                    }
+                }).sort({ newprice: sort }).skip(page == 1 ? 0 : (page - 1) * 15).limit(15);
+                totalProduct = await ProductModel.find({
+                    $and: [
+                        { category: { $ne: "Laptop" } },
+                        { type },
+                    ],
+                    newprice: {
+                        $gte: min ? min : 0,
+                        ...filterPrice
+                    }
+                });
+            }
+            else {
+                listProducts = await ProductModel.find({
+                    $and: [
+                        { category: { $ne: "Laptop" } },
+                        { type },
+                    ],
+                    newprice: {
+                        $gte: min ? min : 0,
+                        ...filterPrice
+                    }
+                }).skip(page == 1 ? 0 : (page - 1) * 15).limit(15);
+                totalProduct = await ProductModel.find({
+                    $and: [
+                        { category: { $ne: "Laptop" } },
+                        { type },
+                    ],
+                    newprice: {
+                        $gte: min ? min : 0,
+                        ...filterPrice
+                    }
+                });
+            }
+        }
+        else {
+            if (sort) {
+                listProducts = await ProductModel.find({
+                    $and: [
+                        { category: category },
+                        { type },
+                    ],
+                    newprice: {
+                        $gte: min ? min : 0,
+                        ...filterPrice
+                    }
+                }).sort({ newprice: sort }).skip(page == 1 ? 0 : (page - 1) * 15).limit(15);
+                totalProduct = await ProductModel.find({
+                    $and: [
+                        { category: category },
+                        { type },
+                    ],
+                    newprice: {
+                        $gte: min ? min : 0,
+                        ...filterPrice
+                    }
+                });
+            }
+            else {
+                listProducts = await ProductModel.find({
+                    $and: [
+                        { category: category },
+                        { type },
+                    ],
+                    newprice: {
+                        $gte: min ? min : 0,
+                        ...filterPrice
+                    }
+                }).skip(page == 1 ? 0 : (page - 1) * 15).limit(15);
+                totalProduct = await ProductModel.find({
+                    $and: [
+                        { category: category },
+                        { type },
+                    ],
+                    newprice: {
+                        $gte: min ? min : 0,
+                        ...filterPrice
+                    }
+                });
+            }
+        }
+
+        res.json({
+            listProducts,
+            totalProduct: totalProduct.length,
+            productPerPage: listProducts.length,
+            totalPage: totalProduct.length / 15 < 1 ? 1 : parseInt(totalProduct.length / 15) + 1,
+        })
+
+    },
+    getProductById: async (req, res) => {
+        const { id } = req.params;
+        try {
+            const product = await ProductModel.findById(id);
+            res.json(product);
+        }
+        catch (err) {
+            res.status(500).json(err);
+        }
+    },
     postComment: async (req, res) => {
         const { start, content } = req.body;
         // id product
@@ -191,6 +310,66 @@ module.exports = {
             totalComment: cmtproduct.comment.length,
         });
     },
-    createProduct: async (req, res) => {},
-    updateProduct: async (req, res) => {},
+    createProduct : async (req,res)=>{
+        const {quantity,newprice,oldprice,type,category,content,title} = req.body;
+        var image,ListImage;
+        if(req.files){
+            if(req.files.image){
+                 image = "http://localhost:8080/image/product/"+req.files.image[0].originalname;
+            }
+            if(req.files.listimage){
+                 ListImage = req.files.listimage.map((item)=>`http://localhost:8080/image/product/${item.originalname}`);
+            }
+        }
+        try{
+            const product = new ProductModel({
+                quantity,
+                newprice,
+                oldprice,
+                type,
+                category,
+                title,
+                content,
+                image,
+                ListImage,
+            })
+            return res.json(await product.save());
+        }
+        catch(err){
+            return res.json(err);
+        }
+    },
+    updateProduct : async (req,res)=>{
+        const {id,category,type,title,newprice,oldprice,quantity} = req.body;
+        var image,ListImage;
+        if(req.files){
+            if(req.files.image){
+                 image = "http://localhost:8080/image/product/"+req.files.image[0].originalname;
+            }
+            if(req.files.listimage){
+                 ListImage = req.files.listimage.map((item)=>`http://localhost:8080/image/product/${item.originalname}`);
+            }
+        }
+        try{
+            const product = await ProductModel.findById(id);
+            product.category = category;
+            product.title = title;
+            product.newprice = newprice;
+            product.oldprice = oldprice;
+            product.type = type;
+            product.quantity = quantity;
+            if(image){
+                product.image=image;
+            }
+            if(ListImage){
+                product.ListImage=ListImage;
+
+            }
+            console.log(12345);
+           return res.json(await product.save());
+        }   
+        catch(err){
+
+        }
+    },
 };
