967b2f07b0e1ef9e6986c03a2146d47a676d8751
tonghoang <tongduchoang@gmail.com>
2021-12-04 22:57:41 +0700

    handle  getOrder, deleteOrder, cancelOrder, reOrderPayment, getAllOrder, acceptOrder

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -167,10 +167,102 @@ module.exports = {
         }
     },
     // get all order,
-    getOrder: async (req, res) => {},
-    deleteOrder: async (req, res) => {},
-    cancelOrder: async (req, res) => {},
-    reOrderPayment: async (req, res) => {},
-    getAllOrder: async (req, res) => {},
-    acceptOrder: async (req, res) => {},
+    getOrder: async (req, res) => {
+        const { _id } = req.user.data;
+        const order = await OrderModel.find({ id_user: _id });
+        return res.json(order);
+    },
+    deleteOrder : async (req,res)=>{
+        const {_id} = req.body; // id Order 
+        const idUser = req.user.data._id; // id User
+        try{
+            const Order =await OrderModel.findOneAndDelete({
+                id_user : idUser,
+                _id,
+            });
+            return res.json({
+                statusCode : 200,
+                msg : "Đã xóa đơn hàng"
+            })
+        }
+        catch(err){
+            return res.json({
+                statusCode : 404,
+                msg : err,
+            })
+        }
+    },
+    cancelOrder : async (req,res)=>{
+
+        const {_id} = req.body; // id Order 
+        const idUser = req.user.data._id; // id User
+        try{
+            const Order =await OrderModel.findOne({
+                id_user : idUser,
+                _id,
+            });
+            Order.cancelreason="0";
+            await Order.save();
+            return res.json({
+                statusCode : 200,
+                msg : "Đã hủy đơn hàng"
+            })
+        }
+        catch(err){
+            return res.json({
+                statusCode : 404,
+                msg : err,
+            })
+        }
+    },
+    reOrderPayment : async (req,res)=>{
+        const {_id} = req.body; // id Order 
+        const idUser = req.user.data._id; // id User
+        try{
+            const Order =await OrderModel.findOne({
+                id_user : idUser,
+                _id,
+            });
+            Order.status_order=false,
+            await Order.save();
+            return res.json({
+                statusCode : 200,
+                msg : "Đã đặt lại đơn hàng"
+            })
+        }
+        catch(err){
+            return res.json({
+                statusCode : 404,
+                msg : err,
+            })
+        }
+    },
+    getAllOrder : async (req,res)=>{
+        return res.json(await OrderModel.find({}));
+    },
+    acceptOrder : async(req,res)=>{
+        const {_id} = req.body;
+        try{
+            const order = await OrderModel.findById(_id);
+            order.products.forEach(async (item) => {
+                const product = await ProductModel.findById(item._id);
+                if(product.quantity!=0){
+                    product.quantity--;
+                }
+                await product.save();
+            });
+            order.status_order = true;
+            await order.save();
+            return res.json({
+                statusCode : 200,
+                msg : "Duyệt đơn hàng thành công"
+            });
+        }
+        catch(err){
+            return res.json({
+                statusCode : 404,
+                msg : "Duyệt đơn hàng thất bại ",
+            })
+        }
+    }
 };
